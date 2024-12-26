5761991c9625fc4618de7265c12a02297035cf52
ara-zo <jo91kr@gmail.com>
2024-12-26 19:52:58 +0900

    :tada: Initial commit

---
+++ src/main/kotlin/io/hhplus/cleanarchitecture02/CleanArchitecture02Application.kt
@@ -0,0 +1,11 @@
+package io.hhplus.cleanarchitecture02
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CleanArchitecture02Application
+
+fun main(args: Array<String>) {
+    runApplication<CleanArchitecture02Application>(*args)
+}
---
+++ src/test/kotlin/io/hhplus/cleanarchitecture02/CleanArchitecture02ApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.hhplus.cleanarchitecture02
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CleanArchitecture02ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
